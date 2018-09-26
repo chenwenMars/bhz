7360aaafce46bb4920aca09f9fedc4cf6feaec69
chenwen <442185009@qq.com>
2018-09-27 00:10:12 +0800

    新增测试类

---
+++ bhz-com/src/test/java/com/chenwen/test.java
@@ -0,0 +1,25 @@
+package com.chenwen;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author chenwen
+ * @create 2018-09-26 23:51
+ **/
+public class test {
+    public static void main(String[] args) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        //获取当前月第一天：
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MONTH, 0);
+        c.set(Calendar.DAY_OF_MONTH,1);//设置为1号,当前日期既为本月第一天
+        Date d = c.getTime();
+        String first = format.format(d);
+        System.out.println("===============first:"+first);
+    }
+
+
+
+}
