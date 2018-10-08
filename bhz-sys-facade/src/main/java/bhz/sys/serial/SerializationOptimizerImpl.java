b2ca65e0393c45985487e5cf05e87070d909b1c9
chenwen <442185009@qq.com>
2018-10-09 00:11:07 +0800

    10月9日提交

--- bhz-sys-facade/src/main/java/bhz/sys/serial/SerializationOptimizerImpl.java
+++ bhz-sys-facade/src/main/java/bhz/sys/serial/SerializationOptimizerImpl.java
@@ -1,15 +1,15 @@
 package bhz.sys.serial;
 
+import bhz.sys.entity.SysUser;
+import com.alibaba.dubbo.common.serialize.support.SerializationOptimizer;
+
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-import bhz.sys.entity.*;
-
-import com.alibaba.dubbo.common.serialize.support.SerializationOptimizer;
-
 public class SerializationOptimizerImpl implements SerializationOptimizer {
 
+    @Override
     public Collection<Class> getSerializableClasses() {
         List<Class> classes = new LinkedList<Class>();
         //这里可以把所有需要进行序列化的类进行添加
